ec63a34fdd7a6de002f76dd3313c54b4470dfbe4
Soham Bhagat <bhagatsoham14@gmail.com>
2025-08-11 21:12:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jarvis Assistant</h1>
+      <p>Development environment is working!</p>
+    </div>
+  )
+}
+
+export default App
